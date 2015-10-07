8f5bf96457c6724fd0a1fa742ab2e49f4b68f4b3
Simeon Bird <sbird@andrew.cmu.edu>
2015-10-07 17:51:35 -0400

    Rework the decorator so it actually dynamically subclasses the Simulation class to add values. Also do PPMJ to make docstrings include parent classes (shame this isn't done by default).

--- lyasimulation.py
+++ lyasimulation.py
@@ -6,18 +6,22 @@ import numpy as np
 
 class LymanAlphaSim(simulation.Simulation):
     """Specialise the Simulation class for the Lyman alpha forest.
-        This uses the QuickLya star formation module."""
-    def __init__(self, outdir, box, npart, rescale_gamma = False, rescale_amp = 1., rescale_slope = -0.7, seed = 9281110, redshift=99, redend = 2, omegac=0.2408, omegab=0.0472, hubble=0.7, scalar_amp=2.427e-9, ns=0.97, uvb="hm"):
+        This uses the QuickLya star formation module and allows for altering the power spectrum with knots
+        """
+    __doc__ = __doc__+simulation.Simulation.__doc__
+    def __init__(self, outdir, box, npart, knot_pos = (), knot_val = (1.,1.,1.,1.), rescale_gamma = False, rescale_amp = 1., rescale_slope = -0.7, seed = 9281110, redshift=99, redend = 2, omegac=0.2408, omegab=0.0472, hubble=0.7, scalar_amp=2.427e-9, ns=0.97, uvb="hm"):
         #Parameters of the heating rate rescaling to account for helium reionisation
         #Default parameters do nothing
         self.rescale_gamma = rescale_gamma
         self.rescale_amp = rescale_amp
         self.rescale_slope = rescale_slope
-        #TODO: Set up new output directory hierarchy for Lyman alpha simulations
-        new_outdir = outdir
+        #Set up the knot parameters
+        self.knot_pos = knot_pos
+        self.knot_val = knot_val
+        #Set up new output directory hierarchy for Lyman alpha simulations
+        new_outdir = os.path.join(outdir, "1")
         simulation.Simulation.__init__(self, outdir=new_outdir, box=box, npart=npart, seed=seed, redshift=redshift, redend=redend, separate_gas=True, omegac=omegac, omegab=omegab, hubble=hubble, scalar_amp=scalar_amp, ns=ns, uvb=uvb)
         self.camb_times = [9,]+[x for x in np.arange(4.2,1.9,-0.2)]
-
 
     def _feedback_config_options(self, config):
         """Config options specific to the Lyman alpha forest star formation criterion"""
@@ -41,7 +45,7 @@ class LymanAlphaSim(simulation.Simulation):
         redshifts = np.concatenate([[49,9],np.arange(4.2,1.9,-0.2)])
         return 1./(1.+redshifts)
 
-class ParametricShift(object):
+def change_power_spectrum(object):
     """Class to modify """
 
 if __name__ == "__main__":
--- simulation.py
+++ simulation.py
@@ -446,27 +446,29 @@ class Simulation(object):
         #Save a json of ourselves.
         self.txt_description()
 
-#This decorator (function which acts on a class) contains the information
-#specific to using the COMA cluster.
+
 def coma_mpi_decorate(class_name, nproc=256, timelimit=24):
-    """Decorate an mpi_submit function for a given cluster"""
-    def __init__(self, *args, **kwargs):
-        """New init function which also defines cluster parameters"""
-        self.__real_init__(*args, **kwargs)
-        self.memory = 1200
-        self.timelimit = timelimit
-        self.nproc = nproc
+    """This is a class decorator: it creates a new class which subclasses a given class to contain the information
+        specific to using the COMA cluster.
+        __init__ and _queue_directive are subclassed"""
+    newdoc = class_name.__doc__ + """
+    Subclassed for specific properties of the Coma cluster at CMU.
+    __init__ and _queue_directive are changed."""
+    class ComaClass(class_name):
+        """Docstring should be specified in newdoc"""
+        __doc__ = newdoc
+        def __init__(self, *args, **kwargs):
+            super().__init__(self, *args, **kwargs)
+            self.memory = 1200
+            self.timelimit = timelimit
+            self.nproc = nproc
 
-    def new_que_str(self, prefix="#PBS"):
-        """Generate mpi_submit with coma specific parts"""
-        qstring = self._old_queue_directive(prefix)
-        qstring += prefix+" -q amd\n"
-        qstring += prefix+" -l nodes="+str(self.nproc/16)+":ppn=16\n"
-        return qstring
+        def _queue_directive(self, prefix="#PBS"):
+            """Generate mpi_submit with coma specific parts"""
+            qstring = super()._queue_directive(self, prefix)
+            qstring += prefix+" -q amd\n"
+            qstring += prefix+" -l nodes="+str(self.nproc/16)+":ppn=16\n"
+            return qstring
 
-    class_name._old_queue_directive = class_name._queue_directive
-    class_name._queue_directive = new_que_str
-    class_name.__real_init__ = class_name.__init__
-    class_name.__init__ = __init__
-    return class_name
+    return ComaClass
 
