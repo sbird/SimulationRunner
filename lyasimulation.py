31ea19600aebd0a7ff5c48219f43459fa588cbda
Simeon Bird <sbird@andrew.cmu.edu>
2015-10-08 20:58:16 -0400

    Implement changing the power spectrum by a linear knot, and a test.
    With the Lyman alpha simulations, we want to change the power spectrum to allow for arbitrary shapes.
    This does that. The main difficulty was to make sure the new knots were
    accurately interpolated over, for which we have a testcase.

--- lyasimulation.py
+++ lyasimulation.py
@@ -4,6 +4,7 @@ import simulation
 import os
 import numpy as np
 import string
+import scipy.interpolate as interp
 
 class LymanAlphaSim(simulation.Simulation):
     """Specialise the Simulation class for the Lyman alpha forest.
@@ -56,13 +57,55 @@ class LymanAlphaSim(simulation.Simulation):
         redshifts = np.concatenate([[49,9],np.arange(4.2,1.9,-0.2)])
         return 1./(1.+redshifts)
 
-    def change_power_spectrum_knots(self):
-        """Multiply the power spectrum file by a function specified by our four knots.
-        We assume that the power spectrum is linearly interpolated between the knots,
-        so that we preserve additivity:
-        ie, P(k | A =1.1, B=1.1) / P(k | A =1, B=1) == P(k | A =1.1) / P(k | A =1.)+ P(k | B =1.1) / P(k | A =B.)"""
-        #How to handle interpolation?
-        #TODO: THIS NEEDS A TEST
+def change_power_spectrum_knots(knotpos, knotval, matpow):
+    """Multiply the power spectrum file by a function specified by our knots.
+    We assume that the power spectrum is linearly interpolated between the knots,
+    so that we preserve additivity:
+    ie, P(k | A =1.1, B=1.1) / P(k | A =1, B=1) == P(k | A =1.1) / P(k | A =1.)+ P(k | B =1.1) / P(k | A =B.)
+    On scales larger and smaller than the specified knots, the power spectrum is changed by the same factor as the last knot specified.
+    So if the smallest knotval is 1.1, P(k) from k = 0 -> knotpos[0] is multiplied by 1.1.
+    Note that this means that if you want the large scales to be unchanged, you should impose an extra, fixed, knot that stays constant."""
+    #This should catch some cases where we pass the arguments in the wrong order
+    assert np.all([k1 < k1p for (k1, k1p) in zip(knotpos[:-1], knotpos[1:])])
+    assert np.shape(knotval) == np.shape(knotpos)
+    #Split and copy the matter power spectrum
+    kval = np.array(matpow[:,0])
+    pval = np.array(matpow[:,1])
+    #Check that the input makes physical sense
+    assert np.all(knotpos) > 0
+    assert np.all(knotpos) > kval[0] and np.all(knotpos) < kval[-1]
+    assert np.all(knotval) > 0
+    #BOUNDARY CONDITIONS
+    #Add knots at the start and end of the matter power spectrum.
+    #The large scale knot is always 1.
+    #The small-scale knot always follows the last real knot
+    ext_knotpos = np.concatenate([[kval[0]*0.95,],knotpos, [kval[-1]*1.05,] ])
+    ext_knotval = np.concatenate([[knotval[0],],knotval, [knotval[-1],] ])
+    assert np.shape(ext_knotpos) == np.shape(ext_knotval) and np.shape(ext_knotpos) == (np.size(knotval)+2,)
+    #Insert extra power spectrum evaluations at each knot, to make sure we capture those points properly.
+    #Build an interpolator (in log space) to get new Pk values. Only interpolate a subset of Pk for speed
+    i_limits = np.searchsorted(kval, [knotpos[0]*0.66, knotpos[-1]*1.5])
+    (imin, imax) = (np.max([0,i_limits[0]-5]), np.min([len(kval)-1,i_limits[-1]+5]))
+    pint = interp.interp1d(np.log(kval[imin:imax]), np.log(pval[imin:imax]), kind='cubic')
+    #Make sure that points to be inserted are not too close to the already existing ones.
+    #avg_distance = np.mean(np.log(kval[imin+1:imax+1]) - np.log(kval[imin:imax]))
+    #closest = np.array([np.min(np.abs(kn - kval[imin:imax])) for kn in knotpos])
+    #ii = np.where(closest > avg_distance/4)
+    #Also add an extra point in the midpoint of their interval. This helps spline interpolators.
+    locations = np.searchsorted(kval[imin:imax], knotpos)
+    midpoints = (kval[imin:imax][locations] + kval[imin:imax][locations-1])/2.
+    kplocs = np.searchsorted(knotpos, midpoints)
+    ins_knotpos = np.insert(knotpos, kplocs, midpoints)
+    #Now actually add the new points to the Pk array
+    index = np.searchsorted(kval, ins_knotpos)
+    kval = np.insert(kval, index, ins_knotpos)
+    pval = np.insert(pval, index, np.exp(pint(np.log(ins_knotpos))))
+    #Linearly interpolate between these values
+    dpk = interp.interp1d(ext_knotpos, ext_knotval, kind='linear')
+    #Multiply by the knotted power spectrum interpolated to the point given in the power spectrum file.
+    pval *= dpk(kval)
+    #Build something like the original matpow
+    return np.vstack([kval, pval]).T
 
 if __name__ == "__main__":
     LymanAlphaSim = simulation.coma_mpi_decorate(LymanAlphaSim)
