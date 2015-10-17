65501499cee54faa107e9cc866cf2544c718c133
Simeon Bird <sbird@andrew.cmu.edu>
2015-10-17 02:42:58 -0400

    Test DM only case

--- simulation_test.py
+++ simulation_test.py
@@ -7,7 +7,6 @@ import os
 
 def test_full_integration():
     """Create a full simulation snapshot and check it corresponds to the saved results"""
-    #First remove the test directory
     Sim = Simulation("./test1",box = 256,npart = 256, redshift = 99, redend=0)
     Sim.make_simulation()
     #Check the following files were created
@@ -17,4 +16,11 @@ def test_full_integration():
     #Check these files have not changed
     for f in ("_camb_params.ini", "_genic_params.ini", "Config.sh", "gadget3.param"):
         assert filecmp.cmp(os.path.join("./test1/",f), os.path.join("./testdata/test1/",f))
+    #Clean the test directory if test was successful
     #shutil.rmtree("./test1/")
+
+def test_only_DM():
+    """Create a full simulation with no gas"""
+    Sim = Simulation("./test2",box = 256,npart = 256, redshift = 99, redend=0, separate_gas=False)
+    Sim.make_simulation()
+    assert os.path.exists("./test2")
