3d6ed995214fa8c6a198900a36ad94d92a8ed7ba
Simeon Bird <sbird4@jhu.edu>
2016-06-02 17:59:08 -0400

    Add script to prepare the install on travis

---
+++ prepare_travis.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Script to fetch and build dependencies for the tests, for travis.
+
+mkdir depends
+cd depends
+##Get and make CAMB.
+git clone https://github.com/sbird/camb.git
+cd camb
+make camb
+
+#Get and make N-GenIC.
+cd ../
+git clone https://github.com/sbird/S-GenIC.git
+cd S-GenIC
+git submodule update --init --recursive
+make
+
+#Get and make GenPK.
+cd ../
+git clone https://github.com/sbird/GenPK.git
+cd GenPK
+git submodule update --init --recursive
+make
