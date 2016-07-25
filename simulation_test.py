b618c83dfa8239481a33a9485d5e24fad6a07531
Simeon Bird <sbird4@jhu.edu>
2016-07-25 12:45:26 -0400

    Make tests more robust

--- simulation_test.py
+++ simulation_test.py
@@ -9,29 +9,31 @@ from . import simulationics
 
 def test_full_integration():
     """Create a full simulation snapshot and check it corresponds to the saved results"""
-    defaultpath = os.path.dirname(__file__)
-    Sim = simulationics.SimulationICs(outdir=os.path.join(defaultpath, "test1"),box = 256,npart = 256, redshift = 99, code_args={'redend':0,'do_build':False})
+    defaultpath = os.path.join(os.path.dirname(__file__), "test1")
+    Sim = simulationics.SimulationICs(outdir=defaultpath,box = 256,npart = 256, redshift = 99, code_args={'redend':0,'do_build':False})
     Sim.make_simulation()
     #Check the following files were created
-    assert os.path.exists("./test1")
+    assert os.path.exists(defaultpath)
     for ff in ("times.txt", "TREECOOL", "mpi_submit", "camb_linear", "ICS", "output", "camb_linear/ics_matterpow_99.dat", "ICS/PK-DM-256_256_99", "Simulation.json", "SimulationICs.json"):
-        assert os.path.exists(os.path.join("./test1/", ff))
+        assert os.path.exists(os.path.join(defaultpath, ff))
     #Check these files have not changed
+    testdatadir = os.path.join(os.path.dirname(__file__),"testdata/test1/")
     for f in ("_camb_params.ini", "_genic_params.ini"):
-        config_new = configobj.ConfigObj(os.path.join("./test1",f))
-        config_old = configobj.ConfigObj(os.path.join("./testdata/test1/",f))
+        config_new = configobj.ConfigObj(os.path.join(defaultpath,f))
+        config_old = configobj.ConfigObj(os.path.join(testdatadir,f))
         for key in config_old.keys():
             if re.match("/home/spb",config_old[key]):
                 continue
             assert config_old[key] == config_new[key]
     for f in ("Config.sh", "gadget3.param"):
-        assert filecmp.cmp(os.path.join("./test1/",f), os.path.join("./testdata/test1/",f))
+        assert filecmp.cmp(os.path.join(defaultpath,f), os.path.join(testdatadir,f))
     #Clean the test directory if test was successful
-    #shutil.rmtree("./test1/")
+    #shutil.rmtree(defaultpath)
 
 def test_only_DM():
     """Create a full simulation with no gas"""
-    Sim = simulationics.SimulationICs(outdir=os.path.join(os.path.dirname(__file__),"test2"),box = 256,npart = 256, redshift = 99, separate_gas=False, code_args={'redend':0, 'do_build':False})
+    outdir = os.path.join(os.path.dirname(__file__),"test2")
+    Sim = simulationics.SimulationICs(outdir=outdir, box = 256, npart = 256, redshift = 99, separate_gas=False, code_args={'redend':0, 'do_build':False})
     Sim.make_simulation()
-    assert os.path.exists("./test2")
-    #shutil.rmtree("./test1/")
+    assert os.path.exists(outdir)
+    #shutil.rmtree(outdir)
