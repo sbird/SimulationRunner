6736d34f7a72067df6385fae89a5c510bfc3313a
Simeon Bird <sbird4@jhu.edu>
2016-12-01 10:50:03 -0500

    Fixes for MP-Gadget compilation

--- mpsimulation.py
+++ mpsimulation.py
@@ -42,8 +42,9 @@ class MPSimulation(simulation.Simulation):
         Many of the Gadget options are always on, and there is a new PM gravity solver."""
         g_config_filename = os.path.join(self.outdir, self.gadgetconfig)
         with open(g_config_filename,'w') as config:
-            config.write("# off-tree build into $(DESTDIR)\nDESTDIR  = build\n")
-            config.write("MPICC       =   mpicc\nOPTIMIZE =  -fopenmp -O2 -g -Wall\n")
+            config.write("# off-tree build into $(DESTDIR)\nDESTDIR = build\n")
+            config.write("MPICC = mpicc\nMPICXX = mpic++\n")
+            config.write("OPTIMIZE = -fopenmp -O3 -g -Wall -ffast-math -march=native\n")
             config.write("HDF_INCL = \nHDF_LIBS = -lhdf5\nGSL_INCL = \nGSL_LIBS = -lgsl -lgslcblas\n")
             #We may want DENSITY_INDEPENDENT_SPH as well.
             #config.write(prefix+"DENSITY_INDEPENDENT_SPH\n")
