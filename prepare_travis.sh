763bc37d970f1bf33e704ac0de441017356d1e08
Simeon Bird <sbird4@jhu.edu>
2017-12-09 22:02:57 -0500

    Switch to miniconda for travis

--- prepare_travis.sh
+++ prepare_travis.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 #Script to fetch and build dependencies for the tests, for travis.
 
+wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+chmod +x miniconda.sh
+./miniconda.sh -b -p $HOME/miniconda
+export PATH=$HOME/miniconda/bin:$PATH
+conda update --yes conda
+conda create --yes -n test python=3.4
+source $HOME/miniconda/bin/activate test
+conda install --yes -c gfortran_linux-64 bccp nbodykit matplotlib numpy scipy configobj bigfile nose
+
 mkdir tests
 mkdir depends
 cd depends
@@ -8,9 +17,6 @@ cd depends
 git clone https://github.com/sbird/camb.git
 cd camb
 cd pycamb
-alias gfortran=gfortran-6
-#Maybe this works?
-cat "alias gfortran=gfortran-6" >> ~/.bashrc
 python3 setup.py install --user
 
 #Get and make N-GenIC.
