36676a36beba253aa421991328a0a1ae0935045e
Simeon Bird <sbird@ucr.edu>
2018-02-21 17:04:01 -0800

    Use Omega instead of omega for CLASS

--- SimulationRunner/simulationics.py
+++ SimulationRunner/simulationics.py
@@ -119,7 +119,7 @@ class SimulationICs(object):
         #Set the neutrino density and subtract it from omega0
         omeganu = self.m_nu/93.14/self.hubble**2
         omcdm = (self.omega0 - self.omegab) - omeganu
-        gparams = {'h':self.hubble, 'omega_cdm':omcdm,'omega_b': self.omegab, 'omega_k':0, 'n_s': self.ns, 'A_s': self.scalar_amp, 'k_pivot': 2e-3}
+        gparams = {'h':self.hubble, 'Omega_cdm':omcdm,'Omega_b': self.omegab, 'Omega_k':0, 'n_s': self.ns, 'A_s': self.scalar_amp, 'k_pivot': 2e-3}
         gparams['Omega_Lambda'] = 1 - self.omega0
         (mnu1, mnu2, mnu3) = get_neutrino_masses(self.m_nu, self.nu_hierarchy)
         #Set up massive neutrinos
