a952db89d9b943f777b0c76f44639f2a1ab27a0c
Simeon Bird <sbird@ucr.edu>
2018-11-28 16:07:24 -0800

    Add a script for generating spectra to the cluster section

--- SimulationRunner/clusters.py
+++ SimulationRunner/clusters.py
@@ -23,7 +23,7 @@ class ClusterClass:
         name = os.path.basename(os.path.normpath(outdir))
         with open(os.path.join(outdir, "mpi_submit"),'w') as mpis:
             mpis.write("#!/bin/bash\n")
-            mpis.write(self._queue_directive(name, timelimit=self.timelimit))
+            mpis.write(self._queue_directive(name, timelimit=self.timelimit, nproc=self.nproc))
             mpis.write(self._mpi_program(command=self.gadgetexe+" "+self.gadgetparam))
 
     def generate_mpi_submit_genic(self, outdir, extracommand=None):
@@ -33,7 +33,7 @@ class ClusterClass:
         name = os.path.basename(os.path.normpath(outdir))
         with open(os.path.join(outdir, "mpi_submit_genic"),'w') as mpis:
             mpis.write("#!/bin/bash\n")
-            mpis.write(self._queue_directive(name, timelimit=0.5))
+            mpis.write(self._queue_directive(name, timelimit=0.5, nproc=self.nproc))
             mpis.write(self._mpi_program(command=self.genicexe+" "+self.genicparam))
             if extracommand is not None:
                 mpis.write(extracommand+"\n")
@@ -51,12 +51,13 @@ class ClusterClass:
         timestring = str(hr)+":"+str(minute)+":00"
         return timestring
 
-    def _queue_directive(self, name, timelimit, prefix="#PBS"):
+    def _queue_directive(self, name, timelimit, nproc=16, prefix="#PBS"):
         """Write the part of the mpi_submit file that directs the queueing system.
         This is usually specific to a given cluster.
         The prefix argument is a string at the start of each line.
         It separates queueing system directives from normal comments"""
         _ = name
+        _ = nproc
         qstring = prefix+" -j eo\n"
         qstring += prefix+" -m bae\n"
         qstring += prefix+" -M "+self.email+"\n"
@@ -88,11 +89,11 @@ class HipatiaClass(ClusterClass):
         super().__init__(*args, **kwargs)
         self.memory = 2500
 
-    def _queue_directive(self, name, timelimit, prefix="#PBS"):
+    def _queue_directive(self, name, timelimit, nproc=16, prefix="#PBS"):
         """Generate mpi_submit with coma specific parts"""
         qstring = super()._queue_directive(name=name, prefix=prefix, timelimit=timelimit)
-        qstring += prefix+" -l nodes="+str(int(self.nproc/16))+":ppn=16\n"
-        qstring += prefix+" -l mem="+str(int(self.memory*self.nproc/1000))+"g\n"
+        qstring += prefix+" -l nodes="+str(int(nproc/16))+":ppn=16\n"
+        qstring += prefix+" -l mem="+str(int(self.memory*nproc/1000))+"g\n"
         #Pass environment to child processes
         qstring += prefix+" -V\n"
         return qstring
@@ -117,13 +118,13 @@ class MARCCClass(ClusterClass):
         super().__init__(*args, nproc=nproc,timelimit=timelimit, **kwargs)
         self.memory = 5000
 
-    def _queue_directive(self, name, timelimit, prefix="#SBATCH"):
+    def _queue_directive(self, name, timelimit, nproc=48, prefix="#SBATCH"):
         """Generate mpi_submit with coma specific parts"""
         _ = timelimit
         qstring = prefix+" --partition=parallel\n"
         qstring += prefix+" --job-name="+name+"\n"
         qstring += prefix+" --time="+self.timestring(timelimit)+"\n"
-        qstring += prefix+" --nodes="+str(int(self.nproc/24))+"\n"
+        qstring += prefix+" --nodes="+str(int(nproc/24))+"\n"
         #Number of tasks (processes) per node
         qstring += prefix+" --ntasks-per-node=24\n"
         #Number of cpus (threads) per task (process)
@@ -162,13 +163,13 @@ class BIOClass(ClusterClass):
         super().__init__(*args, nproc=nproc,timelimit=timelimit, **kwargs)
         self.memory = 4
 
-    def _queue_directive(self, name, timelimit, prefix="#SBATCH"):
+    def _queue_directive(self, name, timelimit, nproc=256, prefix="#SBATCH"):
         """Generate mpi_submit with coma specific parts"""
         _ = timelimit
         qstring = prefix+" --partition=short\n"
         qstring += prefix+" --job-name="+name+"\n"
         qstring += prefix+" --time="+self.timestring(timelimit)+"\n"
-        qstring += prefix+" --nodes="+str(int(self.nproc/32))+"\n"
+        qstring += prefix+" --nodes="+str(int(nproc/32))+"\n"
         #Number of tasks (processes) per node
         qstring += prefix+" --ntasks-per-node=32\n"
         #Number of cpus (threads) per task (process)
@@ -200,6 +201,18 @@ class BIOClass(ClusterClass):
         Only MP-Gadget pays attention to this."""
         return "-fopenmp -O3 -g -Wall -ffast-math -march=corei7"
 
+    def generate_spectra_submit(self, outdir):
+        """Generate a sample spectra_submit file, which generates artificial spectra.
+        The prefix argument is a string at the start of each line.
+        It separates queueing system directives from normal comments"""
+        name = os.path.basename(os.path.normpath(outdir))
+        with open(os.path.join(outdir, "spectra_submit"),'w') as mpis:
+            mpis.write("#!/bin/bash\n")
+            mpis.write("""#SBATCH --partition=short\n#SBATCH --job-name="""+pdir+"\n")
+            mpis.write("""#SBATCH --time=1:55:00\n#SBATCH --nodes=1\n#SBATCH --ntasks-per-node=1\n#SBATCH --cpus-per-task=32\n#SBATCH --mem-per-cpu=4G\n""")
+            mpis.write( """#SBATCH --mail-type=end\n#SBATCH --mail-user=sbird@ucr.edu\n""")
+            mpis.write("python flux_power.py "+pdir+"/output\n")
+
 class StampedeClass(ClusterClass):
     """Subclassed for Stampede2's Skylake nodes.
     This has 48 cores (96 threads) per node, each with two sockets, shared memory of 192GB per node, 96 GB per socket.
@@ -207,13 +220,13 @@ class StampedeClass(ClusterClass):
     def __init__(self, *args, nproc=2,timelimit=3,**kwargs):
         super().__init__(*args, nproc=nproc,timelimit=timelimit, **kwargs)
 
-    def _queue_directive(self, name, timelimit, prefix="#SBATCH"):
+    def _queue_directive(self, name, timelimit, nproc=2, prefix="#SBATCH"):
         """Generate mpi_submit with stampede specific parts"""
         _ = timelimit
         qstring = prefix+" --partition=skx-normal\n"
         qstring += prefix+" --job-name="+name+"\n"
         qstring += prefix+" --time="+self.timestring(timelimit)+"\n"
-        qstring += prefix+" --nodes="+str(int(self.nproc))+"\n"
+        qstring += prefix+" --nodes="+str(int(nproc))+"\n"
         #Number of tasks (processes) per node:
         #currently optimal is 2 processes per socket.
         qstring += prefix+" --ntasks-per-node=4\n"
@@ -230,6 +243,18 @@ class StampedeClass(ClusterClass):
         qstring += "ibrun "+command+"\n"
         return qstring
 
+    def generate_spectra_submit(self, outdir):
+        """Generate a sample spectra_submit file, which generates artificial spectra.
+        The prefix argument is a string at the start of each line.
+        It separates queueing system directives from normal comments"""
+        name = os.path.basename(os.path.normpath(outdir))
+        with open(os.path.join(outdir, "spectra_submit"),'w') as mpis:
+            mpis.write("#!/bin/bash\n")
+            #Nodes!
+            mpis.write(self._queue_directive(name, timelimit=1, nproc=1))
+            mpis.write("export OMP_NUM_THREADS=48")
+            mpis.write("python3 flux_power.py output")
+
     def cluster_runtime(self):
         """Runtime options for cluster. None."""
         return {}
@@ -242,7 +267,7 @@ class StampedeClass(ClusterClass):
 
 class HypatiaClass(ClusterClass):
     """Subclass for Hypatia cluster in UCL"""
-    def _queue_directive(self, name, timelimit, prefix="#PBS"):
+    def _queue_directive(self, name, timelimit, nproc=256, prefix="#PBS"):
         """Generate Hypatia-specific mpi_submit"""
         _ = timelimit
         qstring = prefix+" -m bae\n"
@@ -250,7 +275,7 @@ class HypatiaClass(ClusterClass):
         qstring += prefix+" -q smp\n"
         qstring += prefix+" -N "+name+"\n"
         qstring += prefix+" -M "+self.email+"\n"
-        qstring += prefix+" -l nodes=1:ppn="+str(self.nproc)+"\n"
+        qstring += prefix+" -l nodes=1:ppn="+str(nproc)+"\n"
         #Pass environment to child processes
         qstring += prefix+" -V\n"
         return qstring
