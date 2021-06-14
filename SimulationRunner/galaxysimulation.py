e4394129b4a9f1c9c52afca387bec0465b956399
Simeon Bird <sbird@ucr.edu>
2021-06-14 11:10:45 -0700

    Generate output from z=5.4 for lyman alpha by default

--- SimulationRunner/galaxysimulation.py
+++ SimulationRunner/galaxysimulation.py
@@ -80,7 +80,7 @@ class GalaxySim(lyasimulation.LymanAlphaSim):
 
     def generate_times(self):
         """Snapshot outputs for lyman alpha"""
-        redshifts = np.arange(4.2, self.redend, -0.2)
+        redshifts = np.arange(5.4, self.redend, -0.2)
         return 1./(1.+redshifts)
 
     def genicfile(self, camb_output):
