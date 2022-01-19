b74218fd1393aa4c2d3375525ab13e796162f2a4
Simeon Bird <spb@ias.edu>
2022-01-18 23:50:53 -0600

    Fix bugs in UVFlucFile regen script

--- SimulationRunner/regen_hi_files.py
+++ SimulationRunner/regen_hi_files.py
@@ -3,21 +3,23 @@ import glob
 import os.path
 import json
 import sys
-from . import make_HI_reionization_table as hi
+import make_HI_reionization_table as hi
 
 def regen(simdir, genic_param = "_genic_params.ini"):
     """Generate a new file for a specific Simulation"""
-    with os.path.join(simdir,"SimulationICs.json") as fd:
+    with open(os.path.join(simdir,"SimulationICs.json")) as fd:
         icparams = json.load(fd)
         hireionz = icparams["hireionz"]
         uvffile = os.path.join(simdir, "UVFluctuationFile")
         #print(os.path.join(simdir, genic_param), uvffile, hireionz)
         hi.generate_zreion_file(paramfile = os.path.join(simdir, genic_param), output = uvffile, redshift = hireionz, resolution = 1.0)
 
-
 if __name__ == "__main__":
-    emudir = sys.argv[0]
-    dirs = glob.glob(os.path.join(emudir, "ns*"))
+    emudir = sys.argv[1]
+    dirs = sorted(glob.glob(os.path.join(emudir, "ns*")))
 
     for sdir in dirs:
-        regen(simdir=sdir)
+        try:
+            regen(simdir=sdir)
+        except IOError:
+            pass
